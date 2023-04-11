38d1d74e0d1e167504fbe0872f323d4d599c16cc
lfalonsoc <lfalonsoc@gmail.com>
2023-04-10 21:07:37 -0500

    Feat(class 41): Add wait_for_db_command

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,20 +1,22 @@
 """
-Sample test
+Sample tests
 """
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
-    # Test the calc module
+    """Test the calc module."""
+
     def test_add_number(self):
-        # Test adding number together
+        """Test adding numbers together."""
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """ Test subtracting numbers."""
-        res = calc.subtract(10, 5)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
