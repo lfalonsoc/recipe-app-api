78e5d1f0f4caf5610474572147db40c7ac98e0a9
Luis Fernando Alonso Castiblanco <lfalonsoc@gmail.com>
2023-11-21 18:00:07 -0500

    Feat(Class 84): Implement recipe detail API

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -100,5 +100,5 @@ class PrivateRecipeAPITests(TestCase):
         url = detail_url(recipe.id)
         res = self.client.get(url)
 
-        serializer = RecipeSerializer(recipe)
-        self.assertEqual(res.data, serializer,data)
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.data, serializer.data)
