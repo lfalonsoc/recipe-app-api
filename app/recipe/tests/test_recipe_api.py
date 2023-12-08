901f11143a840514e5a92963f9d4a823a08c3add
Luis Fernando Alonso Castiblanco <lfalonsoc@gmail.com>
2023-12-08 11:12:53 -0500

    Feat(class 100): Implement create tag feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -235,7 +235,7 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(recipes.count(), 1)
         recipe = recipes[0]
         self.assertEqual(recipe.tags.count(), 2)
-        self.asserIN(tag_indian, recipe.tags.all())
+        self.assertIn(tag_indian, recipe.tags.all())
         for tag in payload['tags']:
             exists = recipe.tags.filter(
                 name=tag['name'],
