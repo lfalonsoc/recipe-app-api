18f5ca5bd17b983199287d0fd6c70a724a16ea50
Luis Fernando Alonso Castiblanco <lfalonsoc@gmail.com>
2023-12-08 20:24:24 -0500

    Feat(class 109): Write tests for updating ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -54,8 +54,8 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngrediententViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    """Manage ingridients in the database."""
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
     authentication_classes = [TokenAuthentication]
